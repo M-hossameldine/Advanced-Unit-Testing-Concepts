56ba3612274516865be7857d2dfc6ba40df342a5
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-01 00:48:02 +0200

    async-test(promises): add generateTokenPromise() method test suite

--- async/async-example.test.js
+++ async/async-example.test.js
@@ -1,15 +1,15 @@
 import { describe, it, expect } from 'vitest';
 
-import { generateToken } from './async-example';
+import { generateToken, generateTokenPromise } from './async-example';
 
 describe('generateToken()', () => {
   // testing async code that uses callback functions
   // done CB function is used in case of the expect method is called inside a callback function because the "Vitest" wouldn't wait for the callback function to be executed, so the assertion wouldn't happen and the test always will be passed, calling done after the assertion will make "Vitest" wait for the callback function to be executed
 
   it('should generate a token value', (done) => {
-    let email = 'test@gmail.com';
+    let userTestEmail = 'test@test.com';
 
-    generateToken(email, (err, token) => {
+    generateToken(userTestEmail, (err, token) => {
       try {
         expect(token).toBeDefined();
         // expect(token).toBe(2); // to test error case
@@ -18,5 +18,25 @@ describe('generateToken()', () => {
         done(err);
       }
     });
+  });
+});
+
+// testing Async-Code that uses promises
+describe('generateTokenPromise()', () => {
+  // 1st way - using resolves/rejects
+  it('should generate a value token', () => {
+    let userTestEmail = 'test@test.com';
+
+    // using return will make sure that "vitest" waits for the promise to be resolved
+    return expect(generateTokenPromise(userTestEmail)).resolves.toBeDefined();
+  });
+
+  // 2nd way - using async-await
+  it('should generate a value token', async () => {
+    let userTestEmail = 'test@test.com';
+
+    let token = await generateTokenPromise(userTestEmail);
+
+    expect(token).toBeDefined();
   });
 });
